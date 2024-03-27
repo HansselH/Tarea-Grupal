79f70e1c44ee0d2e7f7a2e65b4168f8f4f7727a7
HansselH <hhernandez25g@miumg.edu.gt>
2024-03-26 20:32:54 -0600

    se agrega modificacion del programa 10

--- Programa_10.cpp
+++ Programa_10.cpp
@@ -1,12 +1,49 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-void Programa_10(){
-	int num1=0, num2=0, res=0;
-	cout<<"Ingrese el primer numero a multiplicar: ";
-	cin>>num1;
-	cout<<"Ingrese el segundo numero: ";
-	cin>>num2;
-	res=num1*num2;
-	cout<<"\n  "<<num1<<"\n"<<"X "<<num2<<"\n"<<"-------"<<"\n"<<res<<endl;	
-	system("pause");
+
+void mostrarMultiplicacionGrafica(int num1, int num2) {
+    int resultado = num1 * num2;
+
+    // Imprimir los números y el signo de multiplicación
+    cout << "  " << num1 << endl;
+    cout << "x " << num2 << endl;
+    cout << "----" << endl;
+
+    // Realizar la multiplicación gráficamente
+    int temp = num2;
+    int espacios = 0;
+    while (temp > 0) {
+        int digito = temp % 10;
+        int multiplicacion = num1 * digito;
+        cout << multiplicacion;
+        for (int i = 0; i < espacios; ++i)
+            cout << " ";
+        cout << endl;
+        temp /= 10;
+        espacios++;
+    }
+
+    // Línea divisoria
+    cout << "----" << endl;
+    // Resultado final
+    cout << resultado << endl;
+}
+
+void Programa_10() {
+    int num1, num2;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    cout << "El resultado de la multiplicacion es: " << num1 * num2 << endl;
+
+    // Mostrar la multiplicación gráficamente
+    cout << "\nMultiplicacion grafica:\n" << endl;
+    mostrarMultiplicacionGrafica(num1, num2);
+
+    system("pause"); 
 }
